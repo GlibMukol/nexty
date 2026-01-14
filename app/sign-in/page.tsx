50be023fac16e24d0fc460ca89d1c511f3e3b4fb
mykola <gray.grifon@gmail.com>
2026-01-14 11:19:07 +0100

    add init migration with product table

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function SignInPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div>
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-50 to-red-100">
+            <div className="max-w-md w-full space-y-8">
                 <SignIn />
                 <Link href="/">Go Home</Link>
             </div>
