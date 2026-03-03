04a05a1acef7c95c8e5a59f40f14da12ba8b3581
mykola <gray.grifon@gmail.com>
2026-03-03 16:28:01 +0100

    add snapshot test for add-product loading

---
+++ app/add-product/test/loading.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { it, describe, expect, jest } from "@jest/globals";
+import Loading from "../loading";
+
+describe("add product snapshot check", () => {
+    it("should be same snapshot", () => {
+        const { container } = render(<Loading />);
+        expect(container).toMatchSnapshot();
+    })
+});
